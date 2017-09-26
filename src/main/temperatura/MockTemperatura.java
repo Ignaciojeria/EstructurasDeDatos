87909425a1e0d2e7bf0459d22c7e0cfcad4b312c
Vina del Mar <vina@duoc.acad>
2017-09-26 20:09:03 -0300

    Arreglando iteraciones en el filtro de temperaturas maximas y minimas

--- src/main/temperatura/MockTemperatura.java
+++ src/main/temperatura/MockTemperatura.java
@@ -11,10 +11,10 @@ public class MockTemperatura {
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,11).getTime(),25));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,12).getTime(),19));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,13).getTime(),27));
-	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,14).getTime(),29));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,15).getTime(),22));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,16).getTime(),30));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,17).getTime(),-1));
+	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,14).getTime(),29));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,18).getTime(),-6));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,19).getTime(),-5));
 	temperaturaService.getListaSimple().addDer(new Temperatura(new GregorianCalendar(2000,03,20).getTime(),-2));
--- src/main/temperatura/TemperaturaService.java
+++ src/main/temperatura/TemperaturaService.java
@@ -23,10 +23,10 @@ public class TemperaturaService {
 	public int temperaturaMax() {
 		int temperaturaMaxima=0;
 		for (int row = 0; row < listaSimple.size; row++) {
-			Temperatura temperaturaRow= (Temperatura)listaSimple.get(row);
+		//	Temperatura temperaturaRow= (Temperatura)listaSimple.get(row);
 			for (int col = 0; col < listaSimple.size; col++) {
 			Temperatura temperaturaCol=(Temperatura)listaSimple.get(col);
-				if(temperaturaRow.getTemperatura()<temperaturaCol.getTemperatura())
+				if(temperaturaMaxima<temperaturaCol.getTemperatura())
 				temperaturaMaxima=temperaturaCol.getTemperatura();
 				//else temperaturaMaxima=temperaturaRow.getTemperatura();				
 			}
@@ -37,12 +37,12 @@ public class TemperaturaService {
 	public int TemperaturaMin() {
 		int temperaturaMinima=0;
 		for (int row = 0; row < listaSimple.size; row++) {
-			Temperatura temperaturaRow= (Temperatura)listaSimple.get(row);
+			//Temperatura temperaturaRow= (Temperatura)listaSimple.get(row);
 			for (int col = 0; col < listaSimple.size; col++) {
 			Temperatura temperaturaCol=(Temperatura)listaSimple.get(col);
-				if(temperaturaRow.getTemperatura()>temperaturaCol.getTemperatura())
+				if(temperaturaMinima>temperaturaCol.getTemperatura())
 				temperaturaMinima=temperaturaCol.getTemperatura();
-				//else temperaturaMaxima=temperaturaRow.getTemperatura();				
+				//else temperaturaMinima=temperaturaRow.getTemperatura();				
 			}
 		}
 		return temperaturaMinima;
